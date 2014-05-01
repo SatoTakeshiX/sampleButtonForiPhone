cd8918beda5d007cae74daefa6feb7164b903d9a
satoutakeshi <sato.takeshi1987@gmail.com>
2014-05-01 21:18:29 +0900

    初回コミット

--- sampleButton/ViewController.m
+++ sampleButton/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *selectSex;
+- (IBAction)changeSex:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -26,4 +29,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)changeSex:(UIButton *)sender {
+    if (sender.selected) {
+        NSLog(@"%@",sender.titleLabel.text);
+        sender.selected = NO;
+        self.label.text = [NSString stringWithFormat:@"あなたの性別は%@です。",sender.titleLabel.text];
+    }else if(sender.selected == NO)
+    {
+        NSLog(@"女です。");
+        sender.selected = YES;
+        self.label.text = @"あなたの性別は女です。";
+    }
+}
 @end
